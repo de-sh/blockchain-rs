39aa3671a5b8cc576b4e0bc748ebceeb15cf7c3d
Devdutt Shenoi <devdutt@outlook.in>
2021-04-16 23:26:04 +0530

    Fix some print styles

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use bytes::Bytes;
+use std::collections::VecDeque;
 use sha2::{Digest, Sha256};
 
+#[derive(Clone)]
 pub struct Block {
-    hash: Bytes,
+    curr_hash: Bytes,
     data: Bytes,
     prev_hash: Bytes,
 }
@@ -10,7 +12,7 @@ pub struct Block {
 impl Block {
     fn new(data: Bytes, prev_hash: Bytes) -> Self {
         Self {
-            hash: Bytes::from(Sha256::digest(&[data.clone(), prev_hash.clone()].concat()).to_vec()),
+            curr_hash: Bytes::from(Sha256::digest(&[data.clone(), prev_hash.clone()].concat()).to_vec()),
             data,
             prev_hash,
         }
@@ -21,34 +23,38 @@ impl Block {
     }
 
     fn get_hash(&self) -> Bytes {
-        self.hash.clone()
+        self.curr_hash.clone()
     }
 
     pub fn details(&self) -> String {
         format!(
-            "hash: {:?}\ndata: {:?}\nprev_hash: {:?}",
-            self.hash, self.data, self.prev_hash
+            "\ncurr_hash: 0x{}\nblck data: {:?}\nprev_hash: 0x{}\n",
+            hex::encode(self.curr_hash.to_vec()),
+            self.data,
+            hex::encode(self.prev_hash.to_vec())
         )
     }
 }
 
 pub struct BlockChain {
-    pub blocks: Vec<Block>,
+    pub blocks: VecDeque<Block>,
     curr_hash: Bytes,
 }
 
 impl BlockChain {
     pub fn new() -> Self {
         let block = Block::genesis();
+        let mut blocks = VecDeque::new();
+        blocks.push_back(block.clone());
         Self {
             curr_hash: block.get_hash(),
-            blocks: vec![block],
+            blocks,
         }
     }
 
     pub fn add_block(&mut self, data: Bytes) {
         let new_block = Block::new(data, self.curr_hash.clone());
         self.curr_hash = new_block.get_hash();
-        self.blocks.push(new_block);
+        self.blocks.push_front(new_block);
     }
 }
